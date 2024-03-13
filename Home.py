50501c01dd826417ae603272569da8414096f83c
Thi Nguyen <ndthi@amazon.com.vn>
2024-03-13 18:33:39 +0700

    init repository

---
+++ Home.py
@@ -0,0 +1,34 @@
+import streamlit as st 
+import Libs as glib 
+from langchain.callbacks import StreamlitCallbackHandler
+from PyPDF2 import PdfReader
+
+st.set_page_config(page_title="Home")
+
+if 'chat_history' not in st.session_state: 
+    st.session_state.chat_history = [] 
+
+
+for message in st.session_state.chat_history: 
+    with st.chat_message(message["role"]): 
+        st.markdown(message["text"]) 
+
+
+uploaded_file = st.file_uploader("Upload your customer support content in PDF")
+input_text = st.text_input("Ask me anything if you need a support") 
+
+docs = []
+if uploaded_file is not None and input_text:
+    st_callback = StreamlitCallbackHandler(st.container())
+    reader = PdfReader(uploaded_file)
+    for page in reader.pages:
+        docs.append(page.extract_text())
+
+    st.session_state.chat_history.append({"role":"user", "text":input_text}) 
+    
+    chat_response = glib.get_rag_chat_response(input_text, docs, st_callback) 
+    
+    with st.chat_message("assistant"): 
+        st.markdown(chat_response) 
+    
+    st.session_state.chat_history.append({"role":"assistant", "text":chat_response}) 
---
+++ Libs.py
@@ -0,0 +1,33 @@
+import os
+from langchain.llms.bedrock import Bedrock
+from langchain.chains import ConversationChain
+from langchain_community.tools.google_jobs import GoogleJobsQueryRun
+from langchain_community.utilities.google_jobs import GoogleJobsAPIWrapper
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_llm(streaming_callback):
+        
+    model_parameter = {"temperature": 0, "top_p": 0, "max_tokens_to_sample": 1000}
+    llm = Bedrock(
+        credentials_profile_name=os.environ.get("BWB_PROFILE_NAME"), 
+        region_name=os.environ.get("BWB_REGION_NAME"), 
+        endpoint_url=os.environ.get("BWB_ENDPOINT_URL"), 
+        model_id="anthropic.claude-v2", 
+        model_kwargs=model_parameter,
+        streaming=True,
+        callbacks=[streaming_callback]
+        ) 
+    
+    return llm
+
+    
+def get_rag_chat_response(question, context, streaming_callback): 
+    llm = get_llm(streaming_callback)
+    system_prompt = """System: You are a helpful customer support assistant. 
+    Only anwser related to customer support content provided
+    This is your customer support content: """ + "<text>" + str(context) + "</text>"""
+    user_prompt = "User: " + question
+    prompt = "System:" + system_prompt + "\n\nHuman: " + user_prompt + "\n\nAssistant:"
+    return llm.invoke(prompt)
